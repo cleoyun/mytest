2f9ae51c5f5c02bca5b56375e89b17b82cc2df4f
Liuyf <cleoyun@gmail.com>
2013-01-25 17:43:07 +0800

    ok2

--- page19.py
+++ page19.py
@@ -4,7 +4,7 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
                         "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 print(movies)
-
+print(1)
 for each_item in movies:
     print(each_item)
     print(each_item)
